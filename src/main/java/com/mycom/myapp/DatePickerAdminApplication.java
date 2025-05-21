e8d4e2a6b28c6728dbb293a50340461a4d0d2375
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-21 17:18:37 +0900

    build: 초기 세팅

---
+++ src/main/java/com/mycom/myapp/DatePickerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatePickerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatePickerAdminApplication.class, args);
+    }
+
+}
