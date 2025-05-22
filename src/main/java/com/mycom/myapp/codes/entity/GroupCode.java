348c3c68a537976c473e7a79eb85184f714cc988
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-22 10:26:51 +0900

    feat: Entity, Dto에 생성자 추가

--- src/main/java/com/mycom/myapp/codes/entity/GroupCode.java
+++ src/main/java/com/mycom/myapp/codes/entity/GroupCode.java
@@ -4,10 +4,16 @@ import org.springframework.data.domain.Persistable;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GroupCode implements Persistable<String> {
 
 	@Id
