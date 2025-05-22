9c02b1aa4114ddaed2c6e73c6b08cd5f74b86a40
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-22 09:45:53 +0900

    fix: Code Entity Layer lombok annotation 추가

--- src/main/java/com/mycom/myapp/codes/entity/Code.java
+++ src/main/java/com/mycom/myapp/codes/entity/Code.java
@@ -4,10 +4,16 @@ import com.mycom.myapp.codes.entity.key.CodeKey;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Code {
 
     @EmbeddedId
